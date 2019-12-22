e9cf964becf4b1bbb5d28174937ba4618c2864ac
HariPrasadSala <hariprasad.sala@gmail.com>
2019-12-22 09:10:55 +0530

    File Organization Section Included

---
+++ AccBlogger/src/com/accenture/blogger/JavaSourceFile.java
@@ -0,0 +1,46 @@
+/*
+ * %W% %E%Firstname Lastname
+ *
+ * Copyright (c) 1993-1996 (Company Name), Inc. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * (Company Name). ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ *
+ * (Company Name) MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY
+ * OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+package com.accenture.blogger;
+
+import java.util.*;
+
+/**
+ *Class description goes here.
+ *
+ * @version1.10 04 Oct 1996
+ * @authorFirstname Lastname
+ */
+class JavaSourceFile {
+	/* A class implementation comment can go here. */
+
+	/** classVar1 documentation comment */
+	/** OR */
+	/**
+	 * Documentation comment
+	 */
+	public static int classVar1;
+
+	/**
+	 * ...method doSomethingElsedocumentation comment...
+	 * @param someParamdescription
+	 */    
+	public void doSomethingElse(Object someParam) {
+		// ...implementation goes here...
+	}
+}
