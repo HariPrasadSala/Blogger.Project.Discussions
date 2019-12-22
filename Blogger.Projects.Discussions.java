6bcd1db548599298ce92d5ebf3dc2e7532218f9a
HariPrasadSala <hariprasad.sala@gmail.com>
2019-12-22 08:20:30 +0530

    Blogger Project Details

---
+++ Blogger.Projects.Discussions.java
@@ -0,0 +1,27 @@
+--------------------------------------
+Blogger Project
+--------------------------------------
+	_______________________________________________________
+	Content
+	_______________________________________________________
+
+		1. Architecture
+			* Progressive Web Apps
+			* Play Framework Java
+			* Spring
+		2. Code Conventions (Code Standards)
+			* Introduction
+			* File Names
+			* File Organization
+			* Indentation
+			* Comments
+			* Declarations
+			* Statements
+			* White Space
+			* Naming Conventions
+			* Programming Practices
+			* Code Example
+	_______________________________________________________
+	2. Code Conventions (Code Standards)
+	_______________________________________________________
+		
